9c4b40660dee0776586932e6583a49f5ab19553c
Shubham Jain <shubham.jain@builder.ai>
2025-05-21 12:23:09 +0530

    code refactor

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,9 +2,7 @@ class StringCalculator
 
   def self.add(numbers)
     return 0 if numbers.empty?
-    if numbers.length == 1
-      numbers.to_i
-    end
+
     numbers.split(",").map(&:to_i).sum
   end
 end
