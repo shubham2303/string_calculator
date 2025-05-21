cac1fa0d722dd0643adbc035354c3faeb30e768f
Shubham Jain <shubham.jain@builder.ai>
2025-05-21 12:41:20 +0530

    handle delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,12 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.split("\n", 2)
+      delimiter = delimiter[2]
+      numbers.split(/#{Regexp.escape(delimiter)}|\n|,/).map(&:to_i).sum
+    else
+      numbers.split(/,|\n/).map(&:to_i).sum
+    end
   end
 end
